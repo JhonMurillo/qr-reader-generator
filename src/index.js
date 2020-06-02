4738c52f8697a49e56e9ff7ba70b33a7cbdd69c6
Jhon Mario Murillo Cordoba <jmurillo@condorlabs.io>
2020-06-01 23:51:22 -0500

    Use react-snap instead of react-snapshot

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+// import { render } from 'react-snapshot';
+import { hydrate, render } from "react-dom";
 import './index.css';
 import { App } from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement);
+} else {
+  render(<App />, rootElement);
+}
+
+// render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
